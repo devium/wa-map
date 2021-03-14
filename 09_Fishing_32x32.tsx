acf1e097c559f7d051ab8ac5bcc25356229cdff8
Georg Guba <georg.guba@gmail.com>
2021-03-14 19:22:12 +0100

    Table collision and .tsx pruning

--- 09_Fishing_32x32.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="09_Fishing_32x32" tilewidth="32" tileheight="32" tilecount="432" columns="16">
- <image source="modern_interiors/32x32/Theme_Sorter_32x32/9_Fishing_32x32.png" width="512" height="864"/>
-</tileset>
