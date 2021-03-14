228bfebfaae2c0b006202fd524296a42b9b55292
devium <devium@users.noreply.github.com>
2021-03-14 18:14:53 +0000

    Deploying to gh-pages from master @ 7a24b34e64f145540e5337aa52baf7e018a0ab2a 🚀

---
+++ 15_Christmas_32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="15_Christmas_32x32" tilewidth="32" tileheight="32" tilecount="272" columns="16">
+ <image source="modern_interiors/32x32/Theme_Sorter_32x32/15_Christmas_32x32.png" width="512" height="544"/>
+</tileset>
