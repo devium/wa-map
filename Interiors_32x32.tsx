1074bee34fe34eb4eb3f259a7eada3f71f40e4e2
Georg Guba <georg.guba@gmail.com>
2021-03-14 17:36:52 +0100

    Test room

---
+++ Interiors_32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Interiors_32x32" tilewidth="32" tileheight="32" tilecount="9584" columns="16">
+ <image source="modern_interiors/32x32/Interiors_32x32.png" width="512" height="19168"/>
+</tileset>
