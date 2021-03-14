ae3446398361c3748f92f599ba9e545c3795980d
devium <devium@users.noreply.github.com>
2021-03-14 18:22:50 +0000

    Deploying to gh-pages from master @ acf1e097c559f7d051ab8ac5bcc25356229cdff8 🚀

--- 16_Grocery_store_32x32.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="16_Grocery_store_32x32" tilewidth="32" tileheight="32" tilecount="1248" columns="16">
- <image source="modern_interiors/32x32/Theme_Sorter_32x32/16_Grocery_store_32x32.png" width="512" height="2496"/>
-</tileset>
